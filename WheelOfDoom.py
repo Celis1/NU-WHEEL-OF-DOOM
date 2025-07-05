662e1f261df92bee6a178ba8c0a6b9b4a9442281
Celis1 <alexcopelotti@outlook.com>
2025-07-05 15:08:45 -0400

    added fps updates and non blocking reading of controller values

--- WheelOfDoom.py
+++ WheelOfDoom.py
@@ -1,11 +1,11 @@
 # from inputs import get_gamepad
 # import pyautogui
 # import pydirectinput
-# import time
+import time
 # import math
 import sounddevice as sd
 import soundfile as sf
-# import threading
+import threading
 
 from my_controller import Controller
 
@@ -14,14 +14,35 @@ class WheelOfDoom(Controller):
 
 # TODO: controller doesnt update if there no change in input
 
+def read_controller_thread(controller):
+
+    while True:
+        controller.read()
+
+
+
 if __name__ == "__main__":
+    fps = 60
+    frame_time = 1.0 / fps  # ~0.0167 seconds per frame
+    last_frame_time = time.time()
+
     controller = Controller()
+
+    # thread this function
+    # values = controller.read()
+    # print(values)
+
+    temp = threading.Thread(target=read_controller_thread, args=(controller,))
+    temp.start()
 
     print("Starting Wheel of Doom controller...")
     while True:
-        values = controller.read()
-        print(values)
+        current_time = time.time()
 
-        controller.rotate_mouse(values['ABS_X'])
+        # cap the frame rate
+        if current_time - last_frame_time >= frame_time:
+            controller.rotate_mouse(controller.current_event['ABS_X'])
+            controller.get_action()
+            print(controller.current_event)
 
-        controller.get_action()
+            last_frame_time = current_time
